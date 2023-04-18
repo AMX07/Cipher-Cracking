08b26c10510d65ff8c004fd45340c4b1d6bb440a
Ansh MIttal <58131483+AMX07@users.noreply.github.com>
2023-04-18 17:44:51 -0400

    Update MonoALpha-new.py

--- MonoALpha-new.py
+++ MonoALpha-new.py
@@ -1,79 +1,46 @@
-from spellchecker import SpellChecker
 from collections import Counter
 import string
 
-ENGLISH_FREQUENCY = {
-    'E': 12.70, 'T': 9.06, 'A': 8.17, 'O': 7.51, 'I': 6.97, 'N': 6.75,
-    'S': 6.33, 'H': 6.09, 'R': 5.99, 'D': 4.25, 'L': 4.03, 'C': 2.78,
-    'U': 2.76, 'M': 2.41, 'W': 2.36, 'F': 2.23, 'G': 2.02, 'Y': 1.97,
-    'P': 1.93, 'B': 1.29, 'V': 0.98, 'K': 0.77, 'J': 0.15, 'X': 0.15,
-    'Q': 0.10, 'Z': 0.07
-}
+def frequency_analysis(cipher_text):
+    letters = string.ascii_lowercase
+    english_letter_freq = {
+        'a': 8.12, 'b': 1.49, 'c': 2.71, 'd': 4.32, 'e': 12.02,
+        'f': 2.30, 'g': 2.03, 'h': 5.92, 'i': 7.31, 'j': 0.10,
+        'k': 0.69, 'l': 3.98, 'm': 2.61, 'n': 6.95, 'o': 7.68,
+        'p': 1.82, 'q': 0.11, 'r': 6.02, 's': 6.28, 't': 9.10,
+        'u': 2.88, 'v': 1.11, 'w': 2.09, 'x': 0.17, 'y': 2.11, 'z': 0.07,
+    }
+    cipher_text = cipher_text.lower()
+    char_count = Counter(c for c in cipher_text if c.isalpha())
+    total_chars = sum(char_count.values())
 
+    cipher_freq = {
+        char: (count / total_chars) * 100 for char, count in char_count.items()
+    }
 
-def calculate_frequency(text):
-    total_chars = len(text)
-    char_counts = Counter(text)
-    return {char: (count / total_chars) * 100 for char, count in char_counts.items()}
+    sorted_english_freq = sorted(english_letter_freq.items(), key=lambda x: x[1], reverse=True)
+    sorted_cipher_freq = sorted(cipher_freq.items(), key=lambda x: x[1], reverse=True)
 
+    substitution_table = {}
+    for (plain_char, _), (cipher_char, _) in zip(sorted_english_freq, sorted_cipher_freq):
+        substitution_table[cipher_char] = plain_char
 
-def match_frequencies(cipher_frequencies):
-    sorted_freq = sorted(ENGLISH_FREQUENCY.items(), key=lambda x: x[1], reverse=True)
-    matched_freq = {}
+    return substitution_table
 
-    for cipher_char, freq in cipher_frequencies.items():
-        min_diff = float('inf')
-        matched_char = None
+def decipher(cipher_text, substitution_table):
+    plain_text = ""
+    for char in cipher_text:
+        if char.isalpha():
+            is_upper = char.isupper()
+            original_char = char.lower()
+            plain_char = substitution_table.get(original_char, original_char)
+            plain_text += plain_char.upper() if is_upper else plain_char
+        else:
+            plain_text += char
+    return plain_text
 
-        for english_char, english_freq in sorted_freq:
-            diff = abs(freq - english_freq)
-            if diff < min_diff:
-                min_diff = diff
-                matched_char = english_char
+cipher_text = "DJ DK C QLXDWI WF SDGDU PCX. XLRLU KQCSLKBDQK, KJXDHDET FXWZ C BDIILE RCKL, BCGL PWE JBLDX FDXKJ GDSJWXO CTCDEKJ JBL LGDU TCUCSJDS LZQDXL. IYXDET JBL RCJJUL, XLRLU KQDLK ZCECTLI JW KJLCU KLSXLJ QUCEK JW JBL LZQDXL’K YUJDZCJL PLCQWE, JBL ILCJB KJCX, CE CXZWXLI KQCSL KJCJDWE PDJB LEWYTB QWPLX JW ILKJXWO CE LEJDXL QUCELJ. QYXKYLI RO JBL LZQDXL’K KDEDKJLX CTLEJK, QXDESLKK ULDC XCSLK BWZL CRWCXI BLX KJCXKBDQ, SYKJWIDCE WF JBL KJWULE QUCEK JBCJ SCE KCGL BLX QLWQUL CEI XLKJWXL FXLLIWZ JW JBL TCUCVO."
 
-        matched_freq[cipher_char] = matched_char
-
-    return matched_freq
-
-
-def decrypt(ciphertext, matched_freq):
-    return ''.join(matched_freq.get(char, char) for char in ciphertext)
-
-
-def is_word_valid(word):
-    spell = SpellChecker()
-    return word.lower() in spell.known([word.lower()])
-
-
-def refine_decryption(ciphertext, decryption, matched_freq):
-    words = decryption.split()
-    for index, word in enumerate(words):
-        if not is_word_valid(word):
-            for i in range(len(word)):
-                cipher_char = ciphertext[word.start() + i]
-                substitution_options = [k for k, v in matched_freq.items() if v == word[i]]
-
-                for option in substitution_options:
-                    temp_decryption = decryption[:word.start() + i] + option + decryption[word.start() + i + 1:]
-                    temp_word = temp_decryption.split()[index]
-
-                    if is_word_valid(temp_word):
-                        decryption = temp_decryption
-                        matched_freq[cipher_char] = option
-                        break
-    return decryption
-
-
-def decrypt_monoalphabetic_cipher(ciphertext):
-    ciphertext = ciphertext.upper()
-    cipher_frequencies = calculate_frequency(ciphertext)
-    matched_freq = match_frequencies(cipher_frequencies)
-    decryption = decrypt(ciphertext, matched_freq)
-    final_decryption = refine_decryption(ciphertext, decryption, matched_freq)
-
-    return final_decryption
-
-
-ciphertext = "DJ DK C QLXDWI WF SDGDU PCX. XLRLU KQCSLKBDQK, KJXDHDET FXWZ C BDIILE RCKL, BCGL PWE JBLDX FDXKJ GDSJWXO CTCDEKJ JBL LGDU TCUCSJDS LZQDXL."
-plaintext = decrypt_monoalphabetic_cipher(ciphertext)
-print(plaintext)
+substitution_table = frequency_analysis(cipher_text)
+plain_text = decipher(cipher_text, substitution_table)
+print(plain_text)
